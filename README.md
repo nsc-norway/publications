48685591cc05daef43cf473da1ab2c3570a98105
Marius Bjoernstad <pmb@fa2k.net>
2022-05-04 12:30:28 +0200

    Create environment with conda + update readme

--- README.md
+++ README.md
@@ -3,3 +3,24 @@ Publications
 
 Publication tracking database for NSC.
 
+The scripts can run in Anaconda, but you can also create a smaller conda environment `pub` by running `conda env create`.
+
+## Usage
+
+First activate the Conda envrionment:
+
+* `conda activate anaconda` OR
+* `conda activate pub` if created with `conda env create`.
+
+Run the scripts with `python`. Use `-h` switch for usage information.
+
+* Add new publications: `add.py DOI_OR_PMID`.
+* Make html file and graph: `make_updated_files.py`.
+
+
+## Advanced usage
+
+* List entries: `ls.py`.
+* Modify publication: `modify.py`. Argument: row ID from LS or DOI.
+* Remove: `rm.py ROW_ID` (give a row ID from ls.py output)
+
--- make_updated_files.py
+++ make_updated_files.py
@@ -1,10 +1,10 @@
-import html
+import htmlgenerator
 
 print("Generating files...")
 
 constant_html = '<p class=\"MsoNormal\" style=\"margin-left:36.0pt;text-indent:-36.0pt\">&nbsp;</p>\n\n<p class=\"MsoNormal\">&nbsp;&nbsp;<img alt=\"\" height=\"333\" src=\"/publications/papers_per_year.png\" style=\"width: 726px; height: 333px;\" width=\"726\" /><br />\n&nbsp;</p>\n\n<p class=\"MsoNormal\" style=\"margin-left: 36pt; text-indent: -36pt;\"><span style=\"mso-ascii-font-family:Cambria;mso-hansi-font-family:Cambria;mso-no-proof:yes\">In case we had missed your publication in the list below, please let us know through our&nbsp;<a href=\"https://nettskjema.uio.no/answer/61221.html\">publication registration form</a>.</span></p>\n\n<p>&nbsp;</p>'
 
-content = constant_html + html.get_html()
+content = constant_html + htmlgenerator.get_html()
 
 with open("index.html", "w", encoding='utf-8') as f:
     f.write("""{
