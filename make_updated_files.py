17d6f3b91ea4e22ee842cd253553743033efd20a
Arvind Sundaram <arvindsu@iMac.local>
2025-08-15 12:18:47 +0200

    about page move

--- make_updated_files.py
+++ make_updated_files.py
@@ -2,7 +2,7 @@ import htmlgenerator
 
 print("Generating files...")
 
-constant_html = '<p class=\"MsoNormal\" style=\"margin-left:36.0pt;text-indent:-36.0pt\">&nbsp;</p>\n\n<p class=\"MsoNormal\">&nbsp;&nbsp;<img alt=\"\" height=\"333\" src=\"/publications/papers_per_year.png\" style=\"width: 726px; height: 333px;\" width=\"726\" /><br />\n&nbsp;</p>\n\n<p class=\"MsoNormal\" style=\"margin-left: 36pt; text-indent: -36pt;\"><span style=\"mso-ascii-font-family:Cambria;mso-hansi-font-family:Cambria;mso-no-proof:yes\">In case we had missed your publication in the list below, please let us know through our&nbsp;<a href=\"https://nettskjema.no/a/205052\">publication registration form</a>.</span></p>\n\n<p>&nbsp;</p>'
+constant_html = '<p class=\"MsoNormal\" style=\"margin-left:36.0pt;text-indent:-36.0pt\">&nbsp;</p>\n\n<p class=\"MsoNormal\">&nbsp;&nbsp;<img alt=\"\" height=\"333\" src=\"/about/publications/papers_per_year.png\" style=\"width: 726px; height: 333px;\" width=\"726\" /><br />\n&nbsp;</p>\n\n<p class=\"MsoNormal\" style=\"margin-left: 36pt; text-indent: -36pt;\"><span style=\"mso-ascii-font-family:Cambria;mso-hansi-font-family:Cambria;mso-no-proof:yes\">In case we had missed your publication in the list below, please let us know through our&nbsp;<a href=\"https://nettskjema.no/a/205052\">publication registration form</a>.</span></p>\n\n<p>&nbsp;</p>'
 
 content = constant_html + htmlgenerator.get_html()
 
